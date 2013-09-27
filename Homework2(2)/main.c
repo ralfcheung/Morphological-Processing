f6ff26d14f55e8a9265c1f8eb9837e1f8c058a01
ralfcheung <ralf3001@gmail.com>
2013-09-27 16:54:51 -0700

    Initial Commit

---
+++ Homework2(2)/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Homework2(2)
+//
+//  Created by Ralf Cheung on 9/27/13.
+//  Copyright (c) 2013 Ralf Cheung. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
